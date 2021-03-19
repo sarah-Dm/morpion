1fcd9d7f2894153db9dfe0b03fe4220f7f3ca72e
Sarah Damag <sarahdamag@gmail.com>
2021-03-19 10:21:23 +0100

    with different color for winMe or winBot

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const $boxes = document.querySelectorAll('.box');
 const me = '❌';
 const bot = '⭕️';
+let fonctionCall = 0;
 
 //commencer le jeu au click sur "Play"
 const $startButton = document.querySelector('button');
@@ -11,12 +12,19 @@ const startGame = () => {
 };
 $startButton.addEventListener('click', () => startGame());
 
+const replay = () => {
+  setTimeout(() => {
+    window.location.reload();
+  }, 2000);
+};
+
 //cocher alternativement avec une croix ou un rond
 //1- fixer une variable qui compte le nbre de fois où on appelle la fonction
-let fonctionCall = 0;
 const checkBox = (event) => {
   //2- faire avancer le curseur à chaque fois que la fonction est appelée
   fonctionCall += 1;
+  //si jeu est terminé (sans victoire)
+  if (fonctionCall === 9) replay();
   //3- lier le rond à nbre paire/ la croix à nbre impair
   if (fonctionCall % 2) {
     //créer des éléments DOM ❌
@@ -35,6 +43,7 @@ $boxes.forEach((box, i) => {
 
 //si les div sont identiques en ligne/colonne/diagonale (contiennent croix ou rond) alors arreter le jeu
 const checkEndGame = () => {
+  let color;
   if (
     $boxes[0].innerText &&
     $boxes[1].innerText &&
@@ -42,9 +51,12 @@ const checkEndGame = () => {
     $boxes[0].innerText === $boxes[1].innerText &&
     $boxes[1].innerText === $boxes[2].innerText
   ) {
-    $boxes[0].classList.add('won');
-    $boxes[1].classList.add('won');
-    $boxes[2].classList.add('won');
+    if ($boxes[0].innerText === me) {
+      color = 'wonMe';
+    } else color = 'wonBot';
+    $boxes[0].classList.add(color);
+    $boxes[1].classList.add(color);
+    $boxes[2].classList.add(color);
   }
   if (
     $boxes[0].innerText &&
@@ -53,9 +65,12 @@ const checkEndGame = () => {
     $boxes[0].innerText === $boxes[3].innerText &&
     $boxes[3].innerText === $boxes[6].innerText
   ) {
-    $boxes[0].classList.add('won');
-    $boxes[3].classList.add('won');
-    $boxes[6].classList.add('won');
+    if ($boxes[0].innerText === me) {
+      color = 'wonMe';
+    } else color = 'wonBot';
+    $boxes[0].classList.add(color);
+    $boxes[3].classList.add(color);
+    $boxes[6].classList.add(color);
   }
   if (
     $boxes[0].innerText &&
@@ -64,9 +79,12 @@ const checkEndGame = () => {
     $boxes[0].innerText === $boxes[4].innerText &&
     $boxes[4].innerText === $boxes[8].innerText
   ) {
-    $boxes[0].classList.add('won');
-    $boxes[4].classList.add('won');
-    $boxes[8].classList.add('won');
+    if ($boxes[0].innerText === me) {
+      color = 'wonMe';
+    } else color = 'wonBot';
+    $boxes[0].classList.add(color);
+    $boxes[4].classList.add(color);
+    $boxes[8].classList.add(color);
   }
   if (
     $boxes[1].innerText &&
@@ -75,9 +93,12 @@ const checkEndGame = () => {
     $boxes[1].innerText === $boxes[4].innerText &&
     $boxes[4].innerText === $boxes[7].innerText
   ) {
-    $boxes[1].classList.add('won');
-    $boxes[4].classList.add('won');
-    $boxes[7].classList.add('won');
+    if ($boxes[1].innerText === me) {
+      color = 'wonMe';
+    } else color = 'wonBot';
+    $boxes[1].classList.add(color);
+    $boxes[4].classList.add(color);
+    $boxes[7].classList.add(color);
   }
   if (
     $boxes[2].innerText &&
@@ -86,9 +107,12 @@ const checkEndGame = () => {
     $boxes[2].innerText === $boxes[5].innerText &&
     $boxes[5].innerText === $boxes[8].innerText
   ) {
-    $boxes[2].classList.add('won');
-    $boxes[5].classList.add('won');
-    $boxes[8].classList.add('won');
+    if ($boxes[2].innerText === me) {
+      color = 'wonMe';
+    } else color = 'wonBot';
+    $boxes[2].classList.add(color);
+    $boxes[5].classList.add(color);
+    $boxes[8].classList.add(color);
   }
   if (
     $boxes[2].innerText &&
@@ -97,9 +121,12 @@ const checkEndGame = () => {
     $boxes[2].innerText === $boxes[4].innerText &&
     $boxes[4].innerText === $boxes[6].innerText
   ) {
-    $boxes[2].classList.add('won');
-    $boxes[4].classList.add('won');
-    $boxes[6].classList.add('won');
+    if ($boxes[2].innerText === me) {
+      color = 'wonMe';
+    } else color = 'wonBot';
+    $boxes[2].classList.add(color);
+    $boxes[4].classList.add(color);
+    $boxes[6].classList.add(color);
   }
   if (
     $boxes[3].innerText &&
@@ -108,8 +135,11 @@ const checkEndGame = () => {
     $boxes[3].innerText === $boxes[4].innerText &&
     $boxes[4].innerText === $boxes[5].innerText
   ) {
-    $boxes[3].classList.add('won');
-    $boxes[4].classList.add('won');
-    $boxes[5].classList.add('won');
+    if ($boxes[3].innerText === me) {
+      color = 'wonMe';
+    } else color = 'wonBot';
+    $boxes[3].classList.add(color);
+    $boxes[4].classList.add(color);
+    $boxes[5].classList.add(color);
   }
 };
